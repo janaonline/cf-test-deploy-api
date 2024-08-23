14e45d41d2cf30a90b994b0aceffb5b850bf0602
Babu Thomas <babupt@gmail..com>
2024-08-23 09:49:49 +0530

    Nginx Conf added, pm2 removed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www/html/cityfinance/cf-test-deploy-api
-pm2 start bin/www --name cf-test-deploy-api
+#cd /var/www/html/cityfinance/cf-test-deploy-api
+#pm2 start bin/www --name cf-test-deploy-api
