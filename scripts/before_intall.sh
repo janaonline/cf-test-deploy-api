79b65313c82add4bbe7a08fb574d794c3ee5f9fe
Babu Thomas <babupt@gmail..com>
2024-08-23 10:30:33 +0530

    Nginx scripts

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ls
+cp webserver/cityfinance.conf /etc/nginx/sites-available/cityfinance.conf
+ln -S /etc/nginx/sites-available/cityfinance.conf /etc/nginx/sites-enabled/cityfinance.conf
+service nginx restart
