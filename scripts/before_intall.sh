2da1814e4d502a1fbfc5707e78f005c461b7f65d
jeevad <jeevananthamcse@gmail.com>
2024-08-27 12:41:29 +0530

    install basic package

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 ls
+
+NODE_VERSION="20" 
+
+# Install NVM (Node Version Manager)
+# echo "Installing NVM..."
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Load NVM
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node.js
+# echo "Installing Node.js..."
+nvm install $NODE_VERSION
+# nvm install 14 # for older UI
+nvm use $NODE_VERSION
+nvm alias default $NODE_VERSION
