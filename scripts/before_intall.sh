428a25e841cb922ae3beda1038195bc59ce657da
Babu Thomas <babupt@gmail..com>
2024-08-30 11:42:54 +0530

    Logs changed

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -7,8 +7,8 @@ cd /home/ubuntu
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 . ~/.nvm/nvm.sh
 touch /home/ubuntu/installstarted.txt
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-nvm alias default $NODE_VERSION
-npm install -g pm2
+nvm install $NODE_VERSION >> /home/ubuntu/nvminstall.txt
+nvm use $NODE_VERSION >> /home/ubuntu/numuse.txt
+nvm alias default $NODE_VERSION >> /home/ubuntu/nvmalias.txt
+npm install -g pm2 >> /home/ubuntu/npminstallpm2.txt
 touch /home/ubuntu/installcompleted.txt
