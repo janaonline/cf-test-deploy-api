b7ea11d19ce791a8137caa7c39efc73111212fcf
Babu Thomas <babupt@gmail..com>
2024-08-30 11:43:53 +0530

    Logs

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -4,8 +4,8 @@ cd /home/ubuntu
 
 NODE_VERSION="20" 
 cd /home/ubuntu
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. ~/.nvm/nvm.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash >> >> /home/ubuntu/curlinstallsh.txt
+. ~/.nvm/nvm.sh >> /home/ubuntu/nvmsh.txt
 touch /home/ubuntu/installstarted.txt
 nvm install $NODE_VERSION >> /home/ubuntu/nvminstall.txt
 nvm use $NODE_VERSION >> /home/ubuntu/numuse.txt
