d9989e1473d0abdd592f435ad6099bfad82572be
Babu Thomas <babupt@gmail..com>
2024-08-30 12:14:07 +0530

    Logs changed

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -6,9 +6,9 @@ NODE_VERSION="20"
 cd /home/ubuntu
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 . ~/.nvm/nvm.sh
-touch /home/ubuntu/installstarted.txt
 nvm install $NODE_VERSION
 nvm use $NODE_VERSION
 nvm alias default $NODE_VERSION
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 npm install -g pm2
-touch /home/ubuntu/installcompleted.txt
