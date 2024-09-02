f27909611f93ceb1d8bdb3d2ae3d8175f8733fe6
Babu Thomas <babupt@gmail..com>
2024-09-02 09:39:52 +0530

    Scripts changed

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
 # Before install
-cd /home/ubuntu
-
-NODE_VERSION="20" 
-cd /home/ubuntu
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install $NODE_VERSION
-nvm use $NODE_VERSION
-nvm alias default $NODE_VERSION
-
-npm install -g pm2
