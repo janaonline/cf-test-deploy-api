7d33bc35683730671d75fcea851c31eab6923c72
Babu Thomas <babupt@gmail..com>
2024-09-03 11:45:25 +0530

    start server script changed - prefixed pm2 path

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/html/cityfinance/cf-test-deploy-api
 npm install
-pm2 start bin/www --name cityfinance-test
+/usr/local/nvm/versions/node/v20.17.0/bin/pm2 start bin/www --name cityfinance-test
