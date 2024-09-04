70a52d7e2717b8e195c93c86708d200d00c019bd
Babu Thomas <babupt@gmail..com>
2024-09-04 20:54:12 +0530

    Start script changed to include npm i

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
 cd /var/www/html/cityfinance/cf-test-deploy-api
-/usr/local/nvm/versions/node/v20.17.0/bin/pm2 start bin/www --name cityfinance-test
+npm i
+pm2 start bin/www --name cityfinance-test
