3df400f37bcc57b323d55175bbbf37e82bdbaee7
Babu Thomas <babupt@gmail..com>
2024-09-04 22:47:37 +0530

    Nginx conf added for testing

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # Before install
+cp webserver/cityfinance.conf /etc/nginx/sites-available/cityfinance.conf
+ln -S /etc/nginx/sites-available/cityfinance.conf /etc/nginx/sites-enabled/cityfinance.conf
+service nginx restart
