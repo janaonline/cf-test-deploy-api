5d89c0285619989f02ab4e88af1c74121a5e4b4d
Babu Thomas <babupt@gmail..com>
2024-09-04 20:37:47 +0530

    Start Server script changed to include node full path

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
 cd /var/www/html/cityfinance/cf-test-deploy-api
 /usr/local/nvm/versions/node/v20.17.0/bin/pm2 start bin/www --name cityfinance-test
