334e91e0ca0c439202b4720b045704630cb18ea5
Babu Thomas <babupt@gmail..com>
2024-09-05 11:47:28 +0530

    start server script changed

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
+export PATH="$PATH:/usr/local/nvm/versions/node/v20.17.0/bin/"
 cd /var/www/html/cityfinance/cf-test-deploy-api
-/usr/local/nvm/versions/node/v20.17.0/bin/pm2 stop all
 npm i
 /usr/local/nvm/versions/node/v20.17.0/bin/pm2 start bin/www --name cityfinance-test
